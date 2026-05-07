0e0c89912098be2006428487213ccd44e93ec4c8
Ethan Weinbrecht <eweinbrecht@lcs.on.ca>
2026-05-07 12:05:40 -0400

    Initial Commit

---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by Ethan Edmond Weinbrecht on 2026-05-07.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HousesListView()
+        }
+    }
+}
